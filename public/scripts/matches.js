4bd5911a1aa077faa24b17602ac260b0a3e261cb
RunnyYolk <nickturner57@gmail.com>
2016-10-08 19:18:27 +0100

    Better matches CSS, lazy loading & slide / fade in cards working nicely

--- public/scripts/matches.js
+++ public/scripts/matches.js
@@ -1,6 +1,7 @@
-
-
 $(document).ready(function(){
+  // $('.ui.fluid.card').animate(
+  //   {opacity: 1}, 700, {transform: translateY('-50px')}, 500, function(){}
+  // )
   $(window).scroll(function(){
     if($(window).scrollTop() >= ($(document).height() - $(window).height())){
       $.post({
@@ -10,28 +11,28 @@ $(document).ready(function(){
           var profilesList = $('.ui.four.stackable.cards');
           var color = ['blue', 'pink', 'yellow'];
           var n = 0;
-          response.nextUsers.forEach(function(user){
+          response.nextUsers.forEach(function(user, i){
             var spokenLangs = ""
             var learningLangs = ""
             user.spokenLanguages.forEach(function(lang, i){
-              spokenLangs += "<p>" + lang + "</p>"
+              spokenLangs += "<li>" + lang + "</li>"
             });
             user.learningLanguages.forEach(function(lang, i){
-              learningLangs += "<p>" + lang + "</p>"
+              learningLangs += "<li>" + lang + "</li>"
             });
             profilesList.append('\
-            <a class="ui fluid card '+ color[n] +'" href="/users/'+ user._id +'/view">\
-                <div class="image">\
+            <a class="ui fluid card '+ color[n] +'" href="/users/'+ user._id +'/view" data-num="'+ i +'">\
+                <div class="image-wrapper">\
                   <img src="'+ user.photos[0] +'"/>\
                 </div>\
                 <div class="content">\
                   <div class="info">\
-                    <h3>'+ user.firstName +' '+ user.lastName +'</h3>\
+                    <h3>'+ user.firstName +' <span class="bold">'+ user.lastName +'</span></h3>\
                     <h4>'+ user.city +', '+ user.country +'</h4>\
                     <div class="speaks-container">\
-                      <h5>speaks</h5>'+ spokenLangs +'</div>\
+                      <h5>speaks</h5><ul>'+ spokenLangs +'</ul></div>\
                     <div class="learning-container">\
-                      <h5>learning</h5>'+ learningLangs +'</div>\
+                      <h5>learning</h5><ul>'+ learningLangs +'</ul></div>\
                   </div>\
                 </div>\
               </a>\
