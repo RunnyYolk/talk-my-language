087064093aa9191ea6d9eaff2d55de081cd5a070
RunnyYolk <nickturner57@gmail.com>
2016-10-10 23:23:39 +0100

    change app.js from app.listen to server.listen

--- app.js
+++ app.js
@@ -489,7 +489,7 @@ app.get('/messages/:_id', isLoggedIn, function(req, res){
 })
 
 // ======== For Heroku ========
-http.listen(process.env.PORT || 8080 , process.env.IP, function(){
+server.listen(process.env.PORT || 8080 , process.env.IP, function(){
 // ======== For Local =========
 // server.listen(27017, process.env.IP, function(){
   console.log('Fire it UP!');
